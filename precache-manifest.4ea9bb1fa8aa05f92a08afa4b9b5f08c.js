fb2eb820f0d5cf2ef0dd957709f5a3108630fef5
Nady Shalaby <nady.shalaby@gmail.com>
2018-11-02 19:59:16 +0200

    Updates

---
+++ precache-manifest.4ea9bb1fa8aa05f92a08afa4b9b5f08c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea931a5b98a97e8b8658d9f6d537329e",
+    "url": "/chathub/static/media/app-logo.ea931a5b.svg"
+  },
+  {
+    "revision": "3dfca64a9bbc6b7be434",
+    "url": "/chathub/static/css/main.db5c8045.chunk.css"
+  },
+  {
+    "revision": "3a240587f9a73ba192cb822a0a39f233",
+    "url": "/chathub/static/media/Flaticon.3a240587.eot"
+  },
+  {
+    "revision": "4249c8e74424f2f2a133",
+    "url": "/chathub/static/js/1.4249c8e7.chunk.js"
+  },
+  {
+    "revision": "2eb26c5e8ffd4612bdd6",
+    "url": "/chathub/static/js/runtime~main.2eb26c5e.js"
+  },
+  {
+    "revision": "1ee966d656f02e2e41912d7622009854",
+    "url": "/chathub/static/media/avatar.1ee966d6.png"
+  },
+  {
+    "revision": "3dfca64a9bbc6b7be434",
+    "url": "/chathub/static/js/main.3dfca64a.chunk.js"
+  },
+  {
+    "revision": "bbb2e063e7177c4eacb60163a39c25fc",
+    "url": "/chathub/static/media/Flaticon.bbb2e063.woff"
+  },
+  {
+    "revision": "85c8dfb07d544ff55ba123d2327eba6c",
+    "url": "/chathub/static/media/Flaticon.85c8dfb0.ttf"
+  },
+  {
+    "revision": "47842220faff678f6af537ac3eb9b169",
+    "url": "/chathub/static/media/Flaticon.47842220.svg"
+  },
+  {
+    "revision": "4249c8e74424f2f2a133",
+    "url": "/chathub/static/css/1.aa3a694b.chunk.css"
+  },
+  {
+    "revision": "fa250709c5159f3c3e02d5319b7bf0ca",
+    "url": "/chathub/index.html"
+  }
+];
